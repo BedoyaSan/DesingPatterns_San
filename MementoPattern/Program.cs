5300273b7ba836c8936dd521023e95ca9afc86e6
San <snbeot25@gmail.com>
2025-11-29 20:22:56 -0500

    Correct folder locations

--- MementoPattern/Program.cs
+++ MementoPattern/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Memento pattern, kinda like undo/redo functionality
+
+using MementoPattern;
+
+TextEditor textEditor = new TextEditor();
+
+TextEditorHistory history = new TextEditorHistory();
+
+textEditor.Write("First edition of this content", DateTime.UtcNow, Guid.NewGuid());
+history.SaveState(textEditor);
+
+textEditor.Write("Second edition of this content", DateTime.UtcNow, Guid.NewGuid());
+history.SaveState(textEditor);
+
+textEditor.Write("Last edition of this content", DateTime.UtcNow, Guid.NewGuid());
+history.SaveState(textEditor);
+
+history.Undo(textEditor);
+
+Console.WriteLine(textEditor.GetContent());
--- MementoPattern/TextEditor.cs
+++ MementoPattern/TextEditor.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DesingPatterns_San
+namespace MementoPattern
 {
     public class TextEditor
     {
